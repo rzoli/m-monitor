1c23547c032de8309af51c79336954d8c98ee720
rz <zoltan.raics@raicszoltan.hu>
2017-01-14 15:34:22 +0100

    More graphs on one plot

--- data_storage.py
+++ data_storage.py
@@ -1,5 +1,6 @@
 import sqlite3,unittest,os,logging,time
 from visexpman.engine.generic import utils
+import datetime,numpy
 
 DBCONNECT_TIMEOUT=5
 
@@ -8,6 +9,7 @@ class DataStorage(object):
         self.root_path=root_path
         db_exists=os.path.exists(self.database_filename)
         self.connect()
+        self.t0='201610301500'#2016 okt 30 3 ora
         if not db_exists:
             self.create_database()
         
@@ -55,8 +57,12 @@ class DataStorage(object):
         return events
         
     def read_raw_events(self):
-        self.cursor.execute("select * from events")
-        rawevents= [line for line in self.cursor.fetchall()]
+        if not hasattr(self,'re'):
+            self.cursor.execute("select * from events")
+            rawevents= [line for line in self.cursor.fetchall()]
+            self.re=rawevents
+        else:
+            rawevents=self.re
         return rawevents
         
     def calculate_timeleft(self):
@@ -85,28 +91,137 @@ class DataStorage(object):
         rawevents= self.read_raw_events()
         now=time.time()
         szopi=len([re for re in rawevents if 'Szoptatas' in re[1] and 'vege' not in re[1] and re[0]>now-86400])
-        pisi=len([re for re in rawevents if 'pisi' in re[1].lower() and re[0]>now-86400])
         kaki=len([re for re in rawevents if 'kaka' in re[1].lower() and re[0]>now-86400])
-        return 'Utobbi 24 oraban/Im Letzten 24 Stunden: {0} szoptatas/Stillen, {1} kaki/Stuhl, {2} pisi/Urin'.format(szopi,kaki,pisi)
-
-    def plot(self):
+        return 'Utobbi 24 oraban: {0} szoptatas | {1} kaki'.format(szopi,kaki)
+        
+    def _utolso_het_szopi_kaki(self,t0):
         rawevents= self.read_raw_events()
         #Szoptatas, kaki over time
-        t0='201610301500'#2016 okt 30 3 ora
-        import time,datetime,numpy
-        t0=time.mktime(datetime.datetime.strptime(t0, '%Y%m%d%H%M%S').timetuple())
-        t0=time.time()-7*86400#Utolso 3 nap
         szopi=[re[0] for re in rawevents if 'Szoptatas' in re[1] and 'vege' not in re[1] and re[0]>t0]
         szopi.sort()
         kaki=[re[0] for re in rawevents if 'kaka' in re[1].lower() and re[0]>t0]
-        kaki.sort()        
-        testsuly=numpy.array([[re[0], float(re[2].split(' ')[0])/1e3] for re in rawevents if 'testsuly' in re[1].lower() and re[0]>t0])
+        kaki.sort()
+        return szopi,kaki,rawevents,t0
+
+    def plot(self):
+        t0=time.time()-7*86400#Utolso 7 nap
+        szopi,kaki,rawevents,t0=self._utolso_het_szopi_kaki(t0)
+        #testsuly=numpy.array([[re[0], float(re[2].split(' ')[0])/1e3] for re in rawevents if 'testsuly' in re[1].lower() and re[0]>t0])
         szopi=(numpy.array(szopi))
         kaki=(numpy.array(kaki))
         m=max(kaki.max(),szopi.max())
         #kaki-=m
         #szopi-=m
-        return kaki[1:],numpy.diff(kaki)/3600.,szopi[1:],numpy.diff(szopi)/3600.,testsuly[:,0],testsuly[:,1]
+        return kaki[1:],numpy.diff(kaki)/3600.,szopi[1:],numpy.diff(szopi)/3600.
+        
+    def napirend(self):
+        now=time.time()
+        t0=now-(int(now)%86400+3600)-6*86400
+        szopi,kaki,rawevents,t0=self._utolso_het_szopi_kaki(t0)
+        sz=numpy.array([t%86400+0*3600 for t in szopi])
+        k=numpy.array([t%86400+0*3600 for t in kaki])
+        sz=numpy.split(sz,numpy.where(numpy.diff(sz)<0)[0]+1)
+        k=numpy.split(k,numpy.where(numpy.diff(k)<0)[0]+1)        
+        szi=[numpy.arange(szii.shape[0]) for szii in sz]
+        ki=[numpy.arange(kii.shape[0]) for kii in k]
+        return sz, szi,k,ki
+        
+    def testsuly(self):
+        rawevents= self.read_raw_events()
+        t0=[re[0] for re in rawevents if re[2]=='megszuletett'][0]
+        testsuly=numpy.array([[re[0], float(re[2].split(' ')[0])] for re in rawevents if 'testsuly' in re[1].lower() and 'Kinga' not in re[1] and re[0]>t0])
+        return testsuly[:,0],testsuly[:,1]
+        
+    def pelenka(self):
+        rawevents= self.read_raw_events()
+        pelenkazas=numpy.array([re[0] for re in rawevents if 'pisi' in re[1].lower() or 'kaki' in re[1].lower() or 'kaka' in re[1].lower()])
+        d0=pelenkazas.min()-pelenkazas.min()%86400
+        now=time.time()
+        now=now-now%86400
+        t=numpy.arange((now-d0)/86400+2)*86400+d0
+        pelenkazas_per_nap=[]
+        for i in range(t.shape[0]-1):
+            n=len([p for p in pelenkazas if p>t[i] and p< t[i+1]])
+            pelenkazas_per_nap.append(n)
+        return t[:-1], numpy.array(pelenkazas_per_nap)
+        
+    def alvas(self):
+        rawevents= self.read_raw_events()
+        event_ts= [re[0] for re in rawevents]
+        event_ts.sort()
+        alvasok=[]
+        for i in range(len(rawevents)):
+            re=rawevents[i]
+            if 'alvas' in re[1].lower() and 'vege' not in re[1].lower() and 'leteve' not in re[1].lower():
+                t=re[0]
+                try:
+                    next=event_ts[event_ts.index(t)+1]
+                except IndexError:
+                    continue
+                idotartam=int((next-t)/60.)
+                if self.timestamp2daytime(t)+idotartam*60>86400:
+                    t1=t
+                    idotartam1=(86400-self.timestamp2daytime(t))/60.
+                    t2=t+86401-self.timestamp2daytime(t)
+                    idotartam2=idotartam-idotartam1
+                    alvasok.append([t1,idotartam1])
+                    alvasok.append([t2,idotartam2])
+                else:
+                    alvasok.append([t,idotartam])
+        alvasok.sort()
+        alvasok=numpy.array(alvasok)
+        #napokra bontas
+        d0=alvasok[:,0].min()
+        d0=d0-d0%86400-3600
+        d1=alvasok[:,0].max()
+        d1=d1-d1%86400+86400
+        dd=(d1-d0)/86400.
+        napok=numpy.arange(dd+1)*86400+d0
+        alvasok_per_nap=[]
+        for i in range(napok.shape[0]-1):
+            v=[datetime.datetime.fromtimestamp(napok[i]), numpy.array([item-numpy.array([int(napok[i]),0]) for item in alvasok if item[0]>napok[i] and item[0]<napok[i+1]])]
+            if v[1].shape[0]>0:
+                v.append(round(v[1][:,1].sum()/60.,1))
+                v[1][:,1]=v[1][:,1].cumsum()
+                #y=numpy.insert(v[1][:,1],0,0)
+                y=v[1][:,1]
+                x=v[1][:,0]
+#                x=numpy.insert(v[1][:,0],v[1][:,0].shape[0],86399)
+                v[1]=numpy.array([x,y]).T
+                alvasok_per_nap.append(v)
+        return alvasok_per_nap
+        
+    def timestamp2daytime(self,ts):
+        d=datetime.datetime.fromtimestamp(ts)
+        return int(d.minute*60+d.hour*3600)
+        
+        
+    def idoeloszlas(self):
+        rawevents= self.read_raw_events()
+        t0=time.time()-168*3600
+        kaki=numpy.array([datetime.datetime.fromtimestamp(self.timestamp2daytime(re[0])) for re in rawevents if 'kaka' in re[1].lower() or 'kaki' in re[1].lower()])
+        szopi=numpy.array([datetime.datetime.fromtimestamp(self.timestamp2daytime(re[0])) for re in rawevents if 'szoptatas' in re[1].lower()])
+        alvas=numpy.array([datetime.datetime.fromtimestamp(self.timestamp2daytime(re[0])) for re in rawevents if 'alvas' in re[1].lower() and 'vege' not in re[1].lower()])
+        return kaki,szopi,alvas
+        
+    def napi_kaki_szopi(self):
+        rawevents= self.read_raw_events()
+        t0=time.time()-14*86400
+        kaki=numpy.array([re[0] for re in rawevents if ('kaka' in re[1].lower() or 'kaki' in re[1].lower()) and re[0]>t0])
+        szopi=numpy.array([re[0] for re in rawevents if 'szoptatas' in re[1].lower() and re[0]>t0])
+        kaki=self.timestamp2daystat(kaki)
+        szopi=self.timestamp2daystat(szopi)
+        return kaki,szopi
+        
+    def timestamp2daystat(self,ts):
+        napits=(ts/86400)*86400
+        napok=list(set(napits))
+        napok.sort()
+        napok_count=[]
+        for n in napok:
+            napok_count.append([datetime.datetime.fromtimestamp(n),len([1 for nts in napits if nts == n])])
+        return numpy.array(napok_count)
+            
 
     def format_events(self,events):
         return [{'date': e[0], 'time': e[1], 'category': e[2], 'note': ' '.join(e[3:-1])} for e in events]
@@ -140,6 +255,11 @@ class TestUser(unittest.TestCase):
     def test_02_plot(self):
         d=DataStorage('')
         d.plot()
+        d.napirend()
+        d.testsuly()
+        d.pelenka()
+        d.alvas()
+        d.napi_kaki_szopi()
         pass
         
         
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-#TODO: szoptatas idokozok vs datum, szoptatas/nap, szoptatasi osszido/nap,szeklet/vizelet kozotti idopont vs datum, pisi/kaka idokoz vs datum
+#TODO: plot tengelyekre felirat, gyors report: nem kell pisi, aktualis alvasido, homerseklet grafikonok napokra bontasa
+#TODO: lassu az egesz, optimalizalas: ne olvassa be az egeszet, csak ha a megfelelo linkre klikkelunk, ami sokaig tart: osszes esemeny beolvasasa majd megjelenitese
 ROOT='/home/rz/codes/m-monitor'
-CATEGORIES=['Szoptatas / Stillen', 'Kaka, pisi / Stuhl und Urin','Szoptatas vege / Ende des Stillens', 'Kaka / Stuhl', 'Pisi / Urin', 'Alvas / Schlaf', 'Alvas vege / Ende des Schlafes', 'Ures pelenka', 'Testsuly / Korpergewicht', 'Homerseklet', 'Magassag', 'Kinga haskorfogat', 'Kinga testsuly', 'Magzat kora', 'Eves', 'Seta', 'Jatek', 'Pelenkazas']
+CATEGORIES=['Szoptatas', 'Kaka', 'Alvas', 'Alvashoz leteve', 'Alvas vege', 'Pisi', 'Kaka, pisi','Szoptatas vege','Ures pelenka', 'Testsuly', 'Homerseklet', 'Magassag', 'Kinga haskorfogat', 'Kinga testsuly', 'Magzat kora', 'Eves', 'Seta', 'Jatek', 'Pelenkazas']
 from visexpman.engine.generic import utils
 import user,numpy,logging,data_storage,os,time,datetime,sys
 from flask import Flask,request,render_template,Response,redirect,url_for,abort,make_response
@@ -38,7 +39,14 @@ def index():
     elif 'torol' in request.form:
         timestamp=time.mktime(datetime.datetime.strptime(request.form['sorszam'], '%Y-%m-%d %H:%M').timetuple())
         d.remove_event(timestamp)
-    events=d.read_events()[::-1]
+    elif 'mutat' in request.form or 'plot_frissit' in request.form:
+        return render_template('plot.html', name='Adel', age=d.calculate_timeleft(),#, weight='3000 g', height='15 cm', 
+                            szopas_ota=d.szopasi_ido())
+    elif 'vissza' in request.form:
+        pass
+    #app.logger.info(1)
+    events=d.read_events()[-500:][::-1]
+    #app.logger.info(2)
     now=time.time()
     return render_template('muci.html', name='Adel', age=d.calculate_timeleft(),#, weight='3000 g', height='15 cm', 
                             szopas_ota=d.szopasi_ido(),utolso24ora=d.utolso24ora(),
@@ -74,46 +82,122 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
-@app.route("/plot.png")
-def plot():
+    
+@app.route("/napirend.png")
+def napirend():
     import datetime
-    import StringIO
-    import random
+    import StringIO,matplotlib
 
     from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
     from matplotlib.figure import Figure
     from matplotlib.dates import DateFormatter
+    font = {'size'   : 6}
 
-    fig=Figure()
-    ax=fig.add_subplot(1,1,1)
-    x=[]
-    y=[]
-    now=datetime.datetime.now()
-    delta=datetime.timedelta(days=1)
+    matplotlib.rc('font', **font)
+    nplots=8
+    fig=Figure(dpi=120, figsize=(6,15))
+    #fig.set_size_inches((20,5))
     d=data_storage.DataStorage(ROOT)
-    tk,kaki,tsz,szopi,st,s=d.plot()
+    
+    ax=fig.add_subplot(nplots,1,1)
+    tk,kaki,tsz,szopi=d.plot()
     tk=numpy.array([datetime.datetime.fromtimestamp(tki) for tki in tk])
     tsz=numpy.array([datetime.datetime.fromtimestamp(tszi) for tszi in tsz])
-    st=numpy.array([datetime.datetime.fromtimestamp(tszi) for tszi in st])
     ax.plot_date(tk,kaki,'o-')
     ax.plot_date(tsz,szopi, 'v-')
-    ax.plot_date(st,s, 'ro-')
-    ax.legend(['Kaki', 'Szopi', 'Suly'])
-#    ax.plot_date(x, y, '-')
+    ax.grid(True)
+    ax.legend(['Kaki', 'Szopi'], loc='upper left', prop={'size':7})
     ax.xaxis.set_major_formatter(DateFormatter('%m.%d %Hh'))
-    fig.autofmt_xdate()
-    #ax=fig.add_subplot(2,1,2)
-    #ax.ylabel('g')
+    #fig.autofmt_xdate()
     
-    #ax.xaxis.set_major_formatter(DateFormatter('%m.%d %H:00'))
+    ax=fig.add_subplot(nplots,1,2)
+    sz,szi,k,ki=d.napirend()
+    for i in range(len(sz)-1,0,-1):
+        ss=numpy.array([datetime.datetime.fromtimestamp(s) for s in sz[i]])
+        ax.plot_date(ss, szi[i],'o' if i == 0 else 'v-', ms=4)
+    ax.legend( map(str,range(len(sz))), loc='upper left', prop={'size':7})
+    ax.xaxis.set_major_formatter(DateFormatter('%H:00'))
+    ax.grid(True)
     
+    ax.set_title('Szopi idok',fontsize=12)
+    ax=fig.add_subplot(nplots,1,3)
+    ax.set_title('Kaki idok',fontsize=12)
+    for i in range(len(k)-1,0,-1):
+        ss=numpy.array([datetime.datetime.fromtimestamp(s) for s in k[i]])
+        ax.plot_date(ss, ki[i],'o' if i == 0 else 'v-', ms=4)
+    ax.legend(map(str,range(len(sz))), loc='upper left', prop={'size':7})
+    ax.xaxis.set_major_formatter(DateFormatter('%H:00'))
+    ax.grid(True)
+    
+    
+    ax=fig.add_subplot(nplots,1,4)
+    a=d.alvas()
+    ax.set_title('Napi alvas idok',fontsize=12)
+    for ai in a[::-1][:6]:
+        ax.plot_date(map(datetime.datetime.fromtimestamp,ai[1][:,0]-3600), ai[1][:,1]/60., 'o-', ms=4)
+        ax.grid(True)
+    ax.xaxis.set_major_formatter(DateFormatter('%H:00'))
+    ax=fig.add_subplot(nplots,1,5)
+    ax.set_title('Napi alvas',fontsize=12)
+    a=a[-14:]
+    x=[i[0] for i in a]
+    y=[i[2] for i in a]
+    ax.plot_date(x, y, 'v-')
+    ax.grid(True)
+    ax.xaxis.set_major_formatter(DateFormatter('%m.%d'))
+    ax=fig.add_subplot(nplots,1,6)
+    ax.set_title('Testsuly',fontsize=12)
+    t,s=d.testsuly()
+    t=numpy.array([datetime.datetime.fromtimestamp(ti) for ti in t])
+    ax.plot_date(t, s,'v-')
+    ax.grid(True)
+    ax.xaxis.set_major_formatter(DateFormatter('%m.%d.'))
+    if 0:
+        ax=fig.add_subplot(nplots,1,7)
+        ax.set_title('Pelenka/nap',fontsize=12)
+        t,s=d.pelenka()
+        t=numpy.array([datetime.datetime.fromtimestamp(ti) for ti in t])
+        ax.plot_date(t, s,'ro-')
+        ax.grid(True)
+        ax.xaxis.set_major_formatter(DateFormatter('%m.%d.'))
+    
+    ax=fig.add_subplot(nplots,1,7)
+    ax.set_title('Szobahomerseklet',fontsize=12)
+    
+    t,temp=read_temp('/home/rz/codes/m-monitor/temp.txt')
+    ax.plot_date(t,temp,'-')
+    t,temp=read_temp('/home/rz/codes/m-monitor/temp_tisztaszoba.txt')
+    ax.plot_date(t,temp,'-')
+    ax.grid(True)
+    ax.legend(['Haloszoba', 'Tisztaszoba'],loc='upper left',prop={'size':7})
+    ax.xaxis.set_major_formatter(DateFormatter('%m.%d. %H:%M'))
+    
+    kaki,szopi=d.napi_kaki_szopi()
+    ax=fig.add_subplot(nplots,1,8)
+    ax.set_title('Kaki,szopi',fontsize=12)
+    ax.xaxis.set_major_formatter(DateFormatter('%m.%d'))
+    ax.plot_date(list(kaki[:,0]),list(kaki[:,1]),'o-')
+    ax.plot_date(list(szopi[:,0]),list(szopi[:,1]),'o-')
+    ax.grid(True)
+    ax.legend(['Kaki','szopi'],loc='upper left',prop={'size':7})
+    fig.tight_layout()
     canvas=FigureCanvas(fig)
     png_output = StringIO.StringIO()
     canvas.print_png(png_output)
     response=make_response(png_output.getvalue())
     response.headers['Content-Type'] = 'image/png'
     return response
+    
+def read_temp(fn):
+    f=open(fn,'rt')
+    txt=f.read()
+    f.close()
+    t0=time.time()-4*86400
+    line_items=[line.split(' ') for line in txt.split('\n')[:-1]]
+    temp_data=numpy.array([[float(l[0]),float(l[-1])] for l in line_items if float(l[0])>t0])    
+    t=numpy.array([datetime.datetime.fromtimestamp(ti) for ti in temp_data[:,0]])
+    return t,temp_data[:,1]
+
 
 if __name__ == "__main__":
     if len(sys.argv)>1:
